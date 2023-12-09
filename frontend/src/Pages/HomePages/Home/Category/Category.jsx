819b3b4ffa4ad78c8566a68a582857aa28e299b6
Md. Rabbinur Muktar <59724876+Rabbinur@users.noreply.github.com>
2023-12-09 11:26:07 +0600

    marquee tag add for announcement

--- frontend/src/Pages/HomePages/Home/Category/Category.jsx
+++ frontend/src/Pages/HomePages/Home/Category/Category.jsx
@@ -3,8 +3,16 @@ import { FaBorderAll } from "react-icons/fa6";
 import CategoryCard from "./CategoryCard";
 
 const Category = () => {
+  const currentDate = new Date();
+  const formattedDate = currentDate.toDateString();
   return (
     <div className="bg-[#f6f9fc] relative py-10">
+      <section className="container mb-5 ">
+        <marquee className="bg-white py-[2px] rounded-xl px-10">
+          {formattedDate} , our all outlets are open. Additionally, our online
+          activities are open and operational.
+        </marquee>
+      </section>
       <section className="container ">
         <div className="flexStart gap-2 ">
           <FaBorderAll size={25} className="text-red-500" />
--- frontend/src/Pages/HomePages/Home/FlashDeals/FlashDeals.jsx
+++ frontend/src/Pages/HomePages/Home/FlashDeals/FlashDeals.jsx
@@ -2,15 +2,7 @@ import { FaBolt } from "react-icons/fa6";
 import FlashCard from "./FlashCard";
 const FlashDeals = () => {
   return (
-    <div className="bg-[#f6f9fc] py-10">
-      <section className="container ">
-        <div className="text-wrapper">
-          <h1>
-            12.12 fest is ongoing by you best product get free shipping delivery
-            charge
-          </h1>
-        </div>
-      </section>
+    <div className="bg-[#f6f9fc] pb-10">
       <section className="container ">
         <div className="flexStart gap-2 ">
           <FaBolt size={25} className="text-red-500" />
